70008c81e60f58a2595b32b07cebfa5425d5ca05
Yu Ling Cheng <cyuling23@gmail.com>
2020-05-16 21:28:54 +0200

    Improve UX when no changes

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,27 +1,26 @@
-import * as ReactDOM from 'react-dom'
-import * as React from 'react'
-import { detailedDiff } from 'deep-object-diff';
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import { detailedDiff } from "deep-object-diff";
 
-import Loader from 'components/Loader';
-import getOldColors from 'services/getOldColors';
-import isColorChange from 'services/isColorChange';
-import updateRemoteColors from 'services/updateRemoteColors';
-import Review from 'views/Review';
+import Loader from "components/Loader";
+import getOldColors from "services/getOldColors";
+import updateRemoteColors from "services/updateRemoteColors";
+import Review from "views/Review";
 
-import './ui.css';
+import "./ui.css";
 
 enum Step {
-  INFO = 'INFO',
-  REVIEW = 'REVIEW',
-  LOADING = 'LOADING',
-  SUCCESS = 'SUCCESS',
-};
+  INFO = "INFO",
+  REVIEW = "REVIEW",
+  LOADING = "LOADING",
+  SUCCESS = "SUCCESS",
+}
 
 interface StateProps {
   step: Step;
   newColors: Object;
   encodedColorsFile: {
-    sha: string
+    sha: string;
   };
   userName: string;
   userEmail: string;
@@ -37,11 +36,11 @@ interface StateProps {
 const initialState = {
   newColors: {},
   encodedColorsFile: {
-    sha: ''
+    sha: "",
   },
   colorDiff: {},
   oldColors: {},
-  PRLink: '',
+  PRLink: "",
 };
 
 class App extends React.Component<{}, StateProps> {
@@ -49,38 +48,41 @@ class App extends React.Component<{}, StateProps> {
     super(props);
     this.state = {
       step: Step.INFO,
-      userName: '',
-      userEmail: '',
-      token: '',
-      repository: '',
-      colorsFilepath: '',
-      branchRef: '',
-      ...initialState
+      userName: "",
+      userEmail: "",
+      token: "",
+      repository: "",
+      colorsFilepath: "",
+      branchRef: "",
+      ...initialState,
     };
   }
 
   handleChange = (value: string) => (event) => {
-    this.setState({ [value]: event.target.value } as Pick<StateProps, keyof StateProps>);
-  }
+    this.setState({ [value]: event.target.value } as Pick<
+      StateProps,
+      keyof StateProps
+    >);
+  };
 
-  sendStyleChanges = event => {
+  sendStyleChanges = (event) => {
     event.preventDefault();
     // Save info to figma storage
     parent.postMessage(
       {
         pluginMessage: {
-          type: 'SAVE_INFO',
+          type: "SAVE_INFO",
           userName: this.state.userName,
           userEmail: this.state.userEmail,
           repository: this.state.repository,
           colorsFilepath: this.state.colorsFilepath,
-          branchRef: this.state.branchRef
-        }
+          branchRef: this.state.branchRef,
+        },
       },
-      '*'
+      "*"
     );
     // Get new colors
-    parent.postMessage({ pluginMessage: { type: 'GET_NEW_COLORS' } }, '*');
+    parent.postMessage({ pluginMessage: { type: "GET_NEW_COLORS" } }, "*");
   };
 
   goToStep = (step: Step) => () => {
@@ -88,12 +90,12 @@ class App extends React.Component<{}, StateProps> {
       this.setState(initialState);
     }
     this.setState({ step });
-  }
+  };
 
-  validateChanges = async event => {
+  validateChanges = async (event) => {
     event.preventDefault();
 
-    console.info('New Colors to send to repo', this.state.newColors);
+    console.info("New Colors to send to repo", this.state.newColors);
 
     //Send colors to Repo
     this.goToStep(Step.LOADING)();
@@ -104,7 +106,7 @@ class App extends React.Component<{}, StateProps> {
       userEmail: this.state.userEmail,
       repository: this.state.repository,
       colorsFilepath: this.state.colorsFilepath,
-      branchRef: this.state.branchRef
+      branchRef: this.state.branchRef,
     });
 
     this.setState({ PRLink });
@@ -112,11 +114,11 @@ class App extends React.Component<{}, StateProps> {
   };
 
   componentDidMount() {
-    onmessage = async event => {
+    onmessage = async (event) => {
       const { pluginMessage } = event.data;
 
       switch (pluginMessage.type) {
-        case 'REHYDRATE_INFO':
+        case "REHYDRATE_INFO":
           // Fill info from localStorage
           if (pluginMessage.name) {
             this.setState({ userName: pluginMessage.name });
@@ -134,8 +136,8 @@ class App extends React.Component<{}, StateProps> {
             this.setState({ branchRef: pluginMessage.branchRef });
           }
           break;
-        case 'NEW_COLORS':
-          this.setState({ newColors: pluginMessage.newColors })
+        case "NEW_COLORS":
+          this.setState({ newColors: pluginMessage.newColors });
           const { oldColors, encodedColorsFile } = await getOldColors(
             this.state.repository,
             this.state.colorsFilepath,
@@ -145,149 +147,173 @@ class App extends React.Component<{}, StateProps> {
           this.setState({ encodedColorsFile });
 
           const colorDiff = detailedDiff(oldColors, this.state.newColors);
-          if (isColorChange(colorDiff)) {
-            this.goToStep(Step.REVIEW)();
-            this.setState({ colorDiff, oldColors });
-          }
+          this.goToStep(Step.REVIEW)();
+          this.setState({ colorDiff, oldColors });
           break;
       }
     };
   }
 
   render() {
-    return <>
-      {this.state.step === Step.INFO && <form id="send-style-changes" onSubmit={this.sendStyleChanges}>
-        <p>
-          This plugin helps you export your local styles (colors...) to the code
-          repository of your project.
-        </p>
-        <p>
-          We'll grab the styles defined in the current Figma project and create a pull
-          request to update the project code.
-        </p>
-        <p className="info-banner">
-          <span className="info-icon">?</span>Step 1/2: Make sure your information are correct
-        </p>
-        <div className="form-container">
-          <p>
-            <label className="text-input-label">Name*
-            <br />
-              <input
-                id="name-input"
-                onChange={this.handleChange('userName')}
-                value={this.state.userName}
-              />
-            </label>
-          </p>
-          <p>
-            <label className="text-input-label">Email*
-            <br />
-              <input
-                id="email-input"
-                onChange={this.handleChange('userEmail')}
-                value={this.state.userEmail}
-              />
-            </label>
-          </p>
-          <p>
-            <label className="text-input-label">
-              Github Personnal Access Token*
-              <br />
-              <input
-                id="token"
-                type="password"
-                onChange={this.handleChange('token')}
-                value={this.state.token}
-              />
-            </label>
-          </p>
-          <p>
-            <label className="text-input-label">
-              Github Repository Name*
-              <br />
-              <input
-                id="repository-input"
-                placeholder="GithubNamespace/repository"
-                onChange={this.handleChange('repository')}
-                value={this.state.repository}
-              />
-            </label>
-          </p>
-          <p>
-            <label className="text-input-label">
-              Colors file path*
-              <br />
-              <input
-                id="colorsfilepath-input"
-                placeholder="src/globals/colors.json"
-                onChange={this.handleChange('colorsFilepath')}
-                value={this.state.colorsFilepath}
-              />
-            </label>
-          </p>
-          <p>
-            <label className="text-input-label"
-            >
-              Branch reference*
-              <br />
-              <input
-                id="branchref-input"
-                placeholder="master"
-                onChange={this.handleChange('branchRef')}
-                value={this.state.branchRef}
-              />
-            </label>
-          </p>
-        </div>
-        <div className="validate-section">
-          <button type="submit" id="send">Export colors</button>
-        </div>
-      </form>
-      }
-      {this.state.step === Step.REVIEW && <div id="confirmation-panel">
-        <Review colorDiff={this.state.colorDiff} oldColors={this.state.oldColors} />
-        <div className="validate-section">
-          <button
-            type="submit"
-            id="back-step-1"
-            className="ghost"
-            onClick={this.goToStep(Step.INFO)}
-          >Back</button>
-          <button type="submit" onClick={this.validateChanges} id="validate">Validate</button>
-        </div>
-      </div>
-      }
-      {this.state.step === Step.SUCCESS && <div id="success-panel">
-        <p className="success-banner">
-          ✅ Your changes were successfully sent! Share your work with the developers
-        </p>
-        <div className="form-container">
-          <a
-            id="pull-request-link"
-            target="_blank"
-            rel="noopener"
-            href={this.state.PRLink}
-          >{this.state.PRLink}</a>
-          <input id="pull-request-input" />
-        </div>
-        <p>
-          <button
-            type="submit"
-            id="back-step-1-bis"
-            className="ghost"
-            onClick={this.goToStep(Step.INFO)}
-          >Back</button>
-          <button id="copy-url-button">Copy the URL</button>
-          <span id="url-copied">URL copied</span>
-        </p>
-      </div>
-      }
-      {this.state.step === Step.LOADING && <div id="loader-panel">
-        <Loader />
-      </div>
-      }
-    </>
+    return (
+      <>
+        {this.state.step === Step.INFO && (
+          <form id="send-style-changes" onSubmit={this.sendStyleChanges}>
+            <p>
+              This plugin helps you export your local styles (colors...) to the
+              code repository of your project.
+            </p>
+            <p>
+              We'll grab the styles defined in the current Figma project and
+              create a pull request to update the project code.
+            </p>
+            <p className="info-banner">
+              <span className="info-icon">?</span>Step 1/2: Make sure your
+              information are correct
+            </p>
+            <div className="form-container">
+              <p>
+                <label className="text-input-label">
+                  Name*
+                  <br />
+                  <input
+                    id="name-input"
+                    onChange={this.handleChange("userName")}
+                    value={this.state.userName}
+                  />
+                </label>
+              </p>
+              <p>
+                <label className="text-input-label">
+                  Email*
+                  <br />
+                  <input
+                    id="email-input"
+                    onChange={this.handleChange("userEmail")}
+                    value={this.state.userEmail}
+                  />
+                </label>
+              </p>
+              <p>
+                <label className="text-input-label">
+                  Github Personnal Access Token*
+                  <br />
+                  <input
+                    id="token"
+                    type="password"
+                    onChange={this.handleChange("token")}
+                    value={this.state.token}
+                  />
+                </label>
+              </p>
+              <p>
+                <label className="text-input-label">
+                  Github Repository Name*
+                  <br />
+                  <input
+                    id="repository-input"
+                    placeholder="GithubNamespace/repository"
+                    onChange={this.handleChange("repository")}
+                    value={this.state.repository}
+                  />
+                </label>
+              </p>
+              <p>
+                <label className="text-input-label">
+                  Colors file path*
+                  <br />
+                  <input
+                    id="colorsfilepath-input"
+                    placeholder="src/globals/colors.json"
+                    onChange={this.handleChange("colorsFilepath")}
+                    value={this.state.colorsFilepath}
+                  />
+                </label>
+              </p>
+              <p>
+                <label className="text-input-label">
+                  Branch reference*
+                  <br />
+                  <input
+                    id="branchref-input"
+                    placeholder="master"
+                    onChange={this.handleChange("branchRef")}
+                    value={this.state.branchRef}
+                  />
+                </label>
+              </p>
+            </div>
+            <div className="validate-section">
+              <button type="submit" id="send">
+                Export colors
+              </button>
+            </div>
+          </form>
+        )}
+        {this.state.step === Step.REVIEW && (
+          <div id="confirmation-panel">
+            <Review
+              colorDiff={this.state.colorDiff}
+              oldColors={this.state.oldColors}
+            />
+            <div className="validate-section">
+              <button
+                type="submit"
+                id="back-step-1"
+                className="ghost"
+                onClick={this.goToStep(Step.INFO)}
+              >
+                Back
+              </button>
+              <button
+                type="submit"
+                onClick={this.validateChanges}
+                id="validate"
+              >
+                Validate
+              </button>
+            </div>
+          </div>
+        )}
+        {this.state.step === Step.SUCCESS && (
+          <div id="success-panel">
+            <p className="success-banner">
+              ✅ Your changes were successfully sent! Share your work with the
+              developers
+            </p>
+            <div className="form-container">
+              <a
+                id="pull-request-link"
+                target="_blank"
+                rel="noopener"
+                href={this.state.PRLink}
+              >
+                {this.state.PRLink}
+              </a>
+              <input id="pull-request-input" />
+            </div>
+            <p>
+              <button
+                type="submit"
+                id="back-step-1-bis"
+                className="ghost"
+                onClick={this.goToStep(Step.INFO)}
+              >
+                Back
+              </button>
+              <button id="copy-url-button">Copy the URL</button>
+              <span id="url-copied">URL copied</span>
+            </p>
+          </div>
+        )}
+        {this.state.step === Step.LOADING && (
+          <div id="loader-panel">
+            <Loader />
+          </div>
+        )}
+      </>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('plugin-body'))
+ReactDOM.render(<App />, document.getElementById("plugin-body"));
--- src/views/Review/index.tsx
+++ src/views/Review/index.tsx
@@ -7,73 +7,94 @@ const TableDiff = ({ changeType, changedColor, colorDiff, oldColors }) => (
       <thead>
         <tr>
           <td className="tone-title">Tone</td>
-          {colorDiff[changeType][changedColor] ?
-            Object.keys(colorDiff[changeType][changedColor]).map(colorTone => (
-              <td key={colorTone}>{colorTone}</td>
-            )) :
-            Object.keys(oldColors[changedColor]).map(colorTone => (
-              <td key={colorTone}>{colorTone}</td>
-            ))
-          }
+          {colorDiff[changeType][changedColor]
+            ? Object.keys(
+                colorDiff[changeType][changedColor]
+              ).map((colorTone) => <td key={colorTone}>{colorTone}</td>)
+            : Object.keys(oldColors[changedColor]).map((colorTone) => (
+                <td key={colorTone}>{colorTone}</td>
+              ))}
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td className={changeType === "deleted" ? "deleted-color" : "added-color"}>
+          <td
+            className={
+              changeType === "deleted" ? "deleted-color" : "added-color"
+            }
+          >
             {changeType === "deleted" ? "Removed" : "New Color"}
           </td>
-          {colorDiff[changeType][changedColor] ?
-            Object.keys(colorDiff[changeType][changedColor]).map(colorTone =>
-              <td
-                key={colorTone}
-                className="color-value"
-                style={{
-                  backgroundColor: colorDiff[changeType][changedColor][colorTone]
-                }}>{colorDiff[changeType][changedColor][colorTone]}</td>
-            ) :
-            Object.keys(oldColors[changedColor]).map(colorTone =>
-              <td
-                key={colorTone}
-                className="color-value"
-                style={{
-                  backgroundColor: oldColors[changedColor][colorTone]
-                }}>{oldColors[changedColor][colorTone]}</td>
-            )
-          }
+          {colorDiff[changeType][changedColor]
+            ? Object.keys(colorDiff[changeType][changedColor]).map(
+                (colorTone) => (
+                  <td
+                    key={colorTone}
+                    className="color-value"
+                    style={{
+                      backgroundColor:
+                        colorDiff[changeType][changedColor][colorTone],
+                    }}
+                  >
+                    {colorDiff[changeType][changedColor][colorTone]}
+                  </td>
+                )
+              )
+            : Object.keys(oldColors[changedColor]).map((colorTone) => (
+                <td
+                  key={colorTone}
+                  className="color-value"
+                  style={{
+                    backgroundColor: oldColors[changedColor][colorTone],
+                  }}
+                >
+                  {oldColors[changedColor][colorTone]}
+                </td>
+              ))}
         </tr>
-        {changeType === "updated" && (<tr>
-          <td className={`${changeType}-color`}>
-            Old Color
-          </td>
-          {Object.keys(colorDiff[changeType][changedColor]).map(colorTone => (
-            <td
-              key={colorTone}
-              className="color-value"
-              style={{
-                backgroundColor: oldColors[changedColor][colorTone]
-              }}>{oldColors[changedColor][colorTone]}
-            </td>
-          ))}
-        </tr>)}
+        {changeType === "updated" && (
+          <tr>
+            <td className={`${changeType}-color`}>Old Color</td>
+            {Object.keys(colorDiff[changeType][changedColor]).map(
+              (colorTone) => (
+                <td
+                  key={colorTone}
+                  className="color-value"
+                  style={{
+                    backgroundColor: oldColors[changedColor][colorTone],
+                  }}
+                >
+                  {oldColors[changedColor][colorTone]}
+                </td>
+              )
+            )}
+          </tr>
+        )}
       </tbody>
     </table>
   </>
 );
 
-const Diff = ({ changeType, colorDiff, oldColors }) => (
-  <>
-    <h3 className="review-section-title">{changeType}</h3>
-    {Object.keys(colorDiff[changeType]).map(changedColor => (
-      <TableDiff
-        key={changedColor}
-        changeType={changeType}
-        changedColor={changedColor}
-        colorDiff={colorDiff}
-        oldColors={oldColors}
-      />
-    ))}
-  </>
-);
+const Diff = ({ changeType, colorDiff, oldColors }) => {
+  const changes = Object.keys(colorDiff[changeType]);
+  if (changes.length === 0) {
+    return <h3 className="review-section-title">Nothing {changeType}</h3>;
+  }
+  return (
+    <>
+      <h3 className="review-section-title">{changeType}</h3>
+      {changes.map((changedColor) => (
+        <TableDiff
+          key={changedColor}
+          changeType={changeType}
+          changedColor={changedColor}
+          colorDiff={colorDiff}
+          oldColors={oldColors}
+        />
+      ))}
+    </>
+  );
+};
 
 const Review = ({ colorDiff, oldColors }) => {
   if (!colorDiff || !oldColors) return null;
@@ -82,14 +103,19 @@ const Review = ({ colorDiff, oldColors }) => {
       <p className="info-banner">
         <span className="info-icon">?</span>
         Step 2/2: Review your changes
-    </p>
+      </p>
       <div id="review-panel">
-        {Object.keys(colorDiff).map(changeType => (
-          <Diff key={changeType} changeType={changeType} colorDiff={colorDiff} oldColors={oldColors} />
+        {Object.keys(colorDiff).map((changeType) => (
+          <Diff
+            key={changeType}
+            changeType={changeType}
+            colorDiff={colorDiff}
+            oldColors={oldColors}
+          />
         ))}
       </div>
     </>
-  )
+  );
 };
 
 export default Review;
